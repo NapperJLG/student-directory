bfcaf8039f3bc4e95b4f6459a42a5a7de5caaf28
napperJLG@gmail.com <ec2-user@ip-172-31-28-133.us-east-2.compute.internal>
2019-03-29 13:24:33 +0000

    added save_students method

--- directory.rb
+++ directory.rb
@@ -1,21 +1,27 @@
 @students = []
 
-def print_menu
-  puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
+  file.close
 end
 
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = gets.chomp
   end
 end
 
@@ -26,62 +32,32 @@ def interactive_menu
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
 def show_students
   print_header
-  print_students_list
+  print_student_list
   print_footer
 end
 
-def interactive_menu
-  
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    
-    case selection
-    when "1"
-      @students = input_students
-    when "2"
-      print_header
-      print
-      print_footer
-    when "9"
-      exit
-    else
-      puts "I dont know what you meant, try again"
-    end
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
   end
-end
-    
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #get the first name
-  name = gets.chomp
-  
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    puts "enter the date of birth of the student:"
-    dob = gets.chomp
-    puts "Enter the students favourite hobby:"
-    hobby = gets.chomp
-    # add the student hash to the array
-    @students << {name: name, dob: dob, hobby: hobby, cohort: :november}
-    
-    if @students.count == 1
-      puts "Now we have #{@students.count} student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
-    # get another name from the user
-    puts "Enter another student or press return again to finish:"
-    name = gets.chomp
-  end
-#returns the array of students
-@students
 end
 
 def print_header
@@ -89,16 +65,14 @@ def print_header
   puts "-------------"
 end
 
-def print_students_list
-  @students.each.with_index(1) do |student, index|
-    puts "#{index}: #{student[:name]}, #{student[:dob]}, hobby: #{student[:hobby]} (#{student[:cohort]} cohort)" 
-     
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer
-puts "Overall, we have #{@students.count} great students "
+  puts "Overall, we have #{@students.count} great students"
 end
-# Nothing happens until we call the methods
 
 interactive_menu
